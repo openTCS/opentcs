openTCS/opentcs

commit eb3a1eccf5d20e31d8ebf63657facce16feac73c

Remove drive order components from VehicleModel.
Replace the list of ModelComponents in a drive order with the
destination point and the current path. Other Components are not
needed.

Reviewed-by: Stefan Walter <stefan.walter@iml.fraunhofer.de>
Reviewed-by: Martin Grzenia <martin.grzenia@iml.fraunhofer.de>

openTCS-OperationsDesk/src/main/java/org/opentcs/guing/exchange/adapter/OpsDeskVehicleAdapter.java
@@ -12,6 +12,7 @@ import java.util.LinkedList;
 import java.util.List;
 import static java.util.Objects.requireNonNull;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -34,6 +35,8 @@ import org.opentcs.guing.model.elements.PathModel;
 import org.opentcs.guing.model.elements.PointModel;
 import org.opentcs.guing.model.elements.VehicleModel;
 import org.opentcs.guing.transport.orders.TransportOrdersContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * An adapter for vehicles specific to the Operations Desk application.
@@ -42,6 +45,11 @@ import org.opentcs.guing.transport.orders.TransportOrdersContainer;
  */
 public class OpsDeskVehicleAdapter
     extends VehicleAdapter {
+
+  /**
+   * This class's logger.
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(OpsDeskVehicleAdapter.class);
 
   /**
    * Keeps track of the drive order components of vehicles.
@@ -84,10 +92,12 @@ public class OpsDeskVehicleAdapter
         component.removeVehicleModel(vehicleModel);
       }
 
-      List<ModelComponent> c = composeDriveOrderComponents(transportOrder.getCurrentDriveOrder(),
-                                                           vehicle.getRouteProgressIndex(),
-                                                           systemModel);
-      vehicleModel.setDriveOrderComponents(c);
+      vehicleModel.setCurrentDriveOrderPath(getCurrentDriveOrderPath(transportOrder.getCurrentDriveOrder(),
+                                                                     vehicle.getRouteProgressIndex(),
+                                                                     systemModel));
+      vehicleModel.setDriveOrderDestination(getCurrentDriveOrderDestination(transportOrder.getCurrentDriveOrder(),
+                                                                            systemModel));
+
       vehicleModel.setDriveOrderState(transportOrder.getState());
     }
     else {
@@ -96,8 +106,8 @@ public class OpsDeskVehicleAdapter
       for (FigureDecorationDetails component : finishedComponents) {
         component.removeVehicleModel(vehicleModel);
       }
-
-      vehicleModel.setDriveOrderComponents(null);
+      vehicleModel.setCurrentDriveOrderPath(null);
+      vehicleModel.setDriveOrderDestination(null);
     }
   }
 
@@ -108,53 +118,30 @@ public class OpsDeskVehicleAdapter
     if (ref == null) {
       return null;
     }
-//    return objectService.fetchObject(TransportOrder.class, ref);
     return transportOrdersContainer.getTransportOrder(ref.getName()).orElse(null);
   }
 
-  /**
-   * Extracts the left over course elements from a drive order and progress.
-   *
-   * @param driveOrder The <code>DriveOrder</code>.
-   * @param routeProgressIndex Index of the current position in the drive order.
-   * @return List containing the left over course elements or <code>null</code>
-   * if driveOrder is <code>null</code>.
-   */
-  private List<ModelComponent> composeDriveOrderComponents(@Nullable DriveOrder driveOrder,
-                                                           int routeProgressIndex,
-                                                           SystemModel systemModel) {
+  private PathModel getCurrentDriveOrderPath(@Nullable DriveOrder driveOrder,
+                                             int routeProgressIndex,
+                                             SystemModel systemModel) {
     if (driveOrder == null) {
       return null;
     }
+    return driveOrder.getRoute().getSteps().stream()
+        .skip(Math.max(0, routeProgressIndex))
+        .map(step -> step.getPath())
+        .filter(path -> path != null)
+        .findFirst()
+        .map(path -> systemModel.getPathModel(path.getName()))
+        .orElse(null);
+  }
 
-    List<ModelComponent> result = new LinkedList<>();
-    List<Route.Step> lSteps = driveOrder.getRoute().getSteps();
-
-    ProcessAdapter adapter;
-    for (int i = lSteps.size() - 1; i >= 0; i--) {
-      if (i == routeProgressIndex) {
-        break;
-      }
-
-      Route.Step step = lSteps.get(i);
-      Path path = step.getPath();
-      Point point = step.getDestinationPoint();
-      PointModel pointModel = systemModel.getPointModel(point.getName());
-      result.add(0, pointModel);
-
-      if (path != null) {
-        PathModel pathModel = systemModel.getPathModel(path.getName());
-        result.add(0, pathModel);
-      }
+  private PointModel getCurrentDriveOrderDestination(@Nullable DriveOrder driveOrder,
+                                                     SystemModel systemModel) {
+    if (driveOrder == null) {
+      return null;
     }
-
-    TCSObjectReference<?> ref = driveOrder.getDestination().getDestination();
-    ModelComponent pointOrLocationModel = systemModel.getModelComponent(ref.getName());
-    if (pointOrLocationModel != null) {
-      result.add(pointOrLocationModel);
-    }
-
-    return result;
+    return systemModel.getPointModel(driveOrder.getDestination().getDestination().getName());
   }
 
   private Set<FigureDecorationDetails> getDriveOrderComponents(@Nullable DriveOrder driveOrder,

openTCS-OperationsDesk/src/main/java/org/opentcs/guing/application/OpenTCSView.java
@@ -63,6 +63,7 @@ import org.opentcs.components.plantoverview.PluggablePanel;
 import org.opentcs.components.plantoverview.PluggablePanelFactory;
 import org.opentcs.customizations.ApplicationEventBus;
 import org.opentcs.customizations.plantoverview.ApplicationFrame;
+import org.opentcs.data.model.Vehicle;
 import org.opentcs.data.notification.UserNotification;
 import org.opentcs.guing.application.action.ToolBarManager;
 import org.opentcs.guing.application.action.ViewActionMap;
@@ -1715,7 +1716,7 @@ public class OpenTCSView
       setCursor(Cursor.getDefaultCursor());
       vehicleModel = null;
 
-      if (vehicleModel != null && vehicleModel.getDriveOrderComponents() == null) {
+      if (vehicleModel != null && (Vehicle.ProcState) vehicleModel.getPropertyProcState().getValue() == Vehicle.ProcState.IDLE) {
         createOrderToPointOnScreen(event.getLocationOnScreen());
       }
     }

openTCS-OperationsDesk/src/main/java/org/opentcs/guing/components/dialogs/SingleVehicleView.java
@@ -19,7 +19,6 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.Arrays;
-import java.util.List;
 import static java.util.Objects.requireNonNull;
 import java.util.ResourceBundle;
 import javax.inject.Inject;
@@ -35,7 +34,7 @@ import org.opentcs.guing.components.properties.event.AttributesChangeEvent;
 import org.opentcs.guing.components.properties.event.AttributesChangeListener;
 import org.opentcs.guing.components.tree.ComponentsTreeViewManager;
 import org.opentcs.guing.components.tree.TreeViewManager;
-import org.opentcs.guing.model.ModelComponent;
+import org.opentcs.guing.model.elements.PointModel;
 import org.opentcs.guing.model.elements.VehicleModel;
 import org.opentcs.guing.persistence.ModelManager;
 import org.opentcs.guing.util.I18nPlantOverviewOperating;
@@ -173,9 +172,9 @@ public class SingleVehicleView
   }
 
   private void updateVehicleDestination() {
-    List<ModelComponent> components = getVehicleModel().getDriveOrderComponents();
-    if (components != null && !components.isEmpty()) {
-      destinationValueLabel.setText(components.get(components.size() - 1).getName());
+    PointModel destinationPoint = getVehicleModel().getDriveOrderDestination();
+    if (destinationPoint != null) {
+      destinationValueLabel.setText(destinationPoint.getName());
     }
     else {
       destinationValueLabel.setText("-");

openTCS-OperationsDesk/src/main/java/org/opentcs/guing/components/drawing/ViewDragScrollListener.java
@@ -24,6 +24,7 @@ import javax.swing.JViewport;
 import javax.swing.SwingUtilities;
 import org.jhotdraw.draw.DrawingView;
 import org.jhotdraw.draw.Figure;
+import org.opentcs.data.model.Vehicle;
 import org.opentcs.guing.application.StatusPanel;
 import org.opentcs.guing.components.drawing.figures.FigureConstants;
 import org.opentcs.guing.components.drawing.figures.LabeledLocationFigure;
@@ -376,7 +377,7 @@ public class ViewDragScrollListener
       LOG.warn("Selected VehicleFigure does not have a model, skipping.");
       return;
     }
-    if (vehicleModel.getDriveOrderComponents() != null) {
+    if ((Vehicle.ProcState) vehicleModel.getPropertyProcState().getValue() != Vehicle.ProcState.IDLE) {
       LOG.debug("Selected vehicle already has an order, skipping.");
       return;
     }

openTCS-OperationsDesk/src/main/java/org/opentcs/guing/components/drawing/figures/VehicleFigure.java
@@ -327,15 +327,7 @@ public class VehicleFigure
 
     AbstractConnection connection;
     if (model.getDriveOrderState() == TransportOrder.State.BEING_PROCESSED) {
-      if (model.getDriveOrderComponents() == null) {
-        connection = null;
-      }
-      else {
-        connection = (PathModel) model.getDriveOrderComponents().stream()
-            .filter(component -> component instanceof PathModel)
-            .findFirst()
-            .orElse(null);
-      }
+      connection = model.getCurrentDriveOrderPath();
     }
     else {
       if (nextPoint != null) {

openTCS-PlantOverview-Base/src/main/java/org/opentcs/guing/model/elements/VehicleModel.java
@@ -9,7 +9,6 @@ package org.opentcs.guing.model.elements;
 
 import java.awt.Color;
 import java.util.Arrays;
-import java.util.List;
 import static java.util.Objects.requireNonNull;
 import java.util.ResourceBundle;
 import javax.annotation.Nonnull;
@@ -31,7 +30,6 @@ import org.opentcs.guing.components.properties.type.StringProperty;
 import org.opentcs.guing.components.properties.type.TripleProperty;
 import org.opentcs.guing.model.AbstractModelComponent;
 import org.opentcs.guing.model.DrawnModelComponent;
-import org.opentcs.guing.model.ModelComponent;
 
 /**
  * Basic implementation of a vehicle. A vehicle has an unique number.
@@ -84,10 +82,6 @@ public class VehicleModel
    */
   private double fOrientationAngle;
   /**
-   * The current drive order.
-   */
-  private List<ModelComponent> fDriveOrderComponents;
-  /**
    * The state of the drive order.
    */
   private TransportOrder.State fDriveOrderState;
@@ -103,6 +97,14 @@ public class VehicleModel
    * A reference to the vehicle.
    */
   private Vehicle vehicle = new Vehicle("Dummy");
+  /**
+   * The current or next path in a drive order.
+   */
+  private PathModel currentDriveOrderPath;
+  /**
+   * The destination for the current drive order.
+   */
+  private PointModel driveOrderDestination;
 
   /**
    * Creates a new instance.
@@ -181,24 +183,6 @@ public class VehicleModel
    */
   public void setOrientationAngle(double angle) {
     fOrientationAngle = angle;
-  }
-
-  /**
-   * Returns a list with all drive order components.
-   *
-   * @return The drive order components.
-   */
-  public List<ModelComponent> getDriveOrderComponents() {
-    return fDriveOrderComponents;
-  }
-
-  /**
-   * Sets the drive order components.
-   *
-   * @param driveOrderComponents A list with the components.
-   */
-  public void setDriveOrderComponents(List<ModelComponent> driveOrderComponents) {
-    fDriveOrderComponents = driveOrderComponents;
   }
 
   /**
@@ -281,6 +265,42 @@ public class VehicleModel
    */
   public void setVehicle(@Nonnull Vehicle vehicle) {
     this.vehicle = requireNonNull(vehicle, "vehicle");
+  }
+
+  /**
+   * Returns the current path for the drive order.
+   *
+   * @return Path for the drive order.
+   */
+  public PathModel getCurrentDriveOrderPath() {
+    return currentDriveOrderPath;
+  }
+
+  /**
+   * Sets the current drive order path.
+   *
+   * @param path the current drive order path.
+   */
+  public void setCurrentDriveOrderPath(PathModel path) {
+    currentDriveOrderPath = path;
+  }
+
+  /**
+   * Returns the destination for the current drive order.
+   *
+   * @return the destination for the current drive order.
+   */
+  public PointModel getDriveOrderDestination() {
+    return driveOrderDestination;
+  }
+
+  /**
+   * Sets the destination for the current drive order.
+   *
+   * @param driveOrderDestination destination for the current drive order.
+   */
+  public void setDriveOrderDestination(PointModel driveOrderDestination) {
+    this.driveOrderDestination = driveOrderDestination;
   }
 
   /**

openTCS-PlantOverview-Common/src/main/java/org/opentcs/guing/exchange/adapter/VehicleAdapter.java
@@ -133,7 +133,8 @@ public class VehicleAdapter
                                        VehicleModel vehicleModel,
                                        SystemModel systemModel)
       throws CredentialsException {
-    vehicleModel.setDriveOrderComponents(null);
+    vehicleModel.setDriveOrderDestination(null);
+    vehicleModel.setCurrentDriveOrderPath(null);
   }
 
   private void updateModelNextPoint(VehicleModel vehicleModel,

commit 1b099027086bd0202843e081e3238144cd8576e0

Fix orientation of vehicles processing orders.
An off-by-one error introduced in
eb3a1eccf5d20e31d8ebf63657facce16feac73c led to selecting the wrong
path on the route for deciding the vehicle's orientation in the
Operations Desk.

Reviewed-by: Stefan Walter <stefan.walter@iml.fraunhofer.de>

openTCS-OperationsDesk/src/main/java/org/opentcs/guing/exchange/adapter/OpsDeskVehicleAdapter.java
@@ -128,7 +128,7 @@ public class OpsDeskVehicleAdapter
       return null;
     }
     return driveOrder.getRoute().getSteps().stream()
-        .skip(Math.max(0, routeProgressIndex))
+        .skip(Math.max(0, routeProgressIndex + 1))
         .map(step -> step.getPath())
         .filter(path -> path != null)
         .findFirst()