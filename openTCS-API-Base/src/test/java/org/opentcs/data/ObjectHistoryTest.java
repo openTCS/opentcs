3c4e1d4564c8da622904b6bf97c8f4c8ed7c2672
Gamze Culhalik <gamze.culhalik@iml.fraunhofer.de>
2023-11-03 15:06:05 +0000

    Add unit tests for object history
    Co-authored-by: Finja Averhaus <finja.averhaus@iml.fraunhofer.de>
    Co-authored-by: Stefan Walter <stefan.walter@iml.fraunhofer.de>
    Merged-by: Stefan Walter <stefan.walter@iml.fraunhofer.de>

---
+++ openTCS-API-Base/src/test/java/org/opentcs/data/ObjectHistoryTest.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) The openTCS Authors.
+ *
+ * This program is free software and subject to the MIT license. (For details,
+ * see the licensing information (LICENSE.txt) you should have received with
+ * this copy of the software.)
+ */
+package org.opentcs.data;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.opentcs.data.ObjectHistory.Entry;
+
+/**
+ * Unit tests for {@link ObjectHistory}.
+ */
+public class ObjectHistoryTest {
+
+  @Test
+  public void checkIfObjectHistoryInitiallyIsEmpty() {
+    ObjectHistory history = new ObjectHistory();
+    assertThat(history.getEntries(), hasSize(0));
+  }
+
+  @Test
+  public void checkIfEntryListIsUnmodifiable() {
+    Entry entry1 = new Entry(Instant.now(), "eventCode1");
+    Entry entry2 = new Entry(Instant.now(), "eventCode2");
+
+    ObjectHistory history = new ObjectHistory().withEntries(List.of(entry1));
+
+    assertThrows(UnsupportedOperationException.class,
+                 () -> history.getEntries().add(entry2));
+  }
+
+  @Test
+  public void testObjectHistoryCreationWithEntries() {
+    Entry entry1 = new Entry(Instant.now(), "eventCode1");
+    Entry entry2 = new Entry(Instant.now(), "eventCode2");
+
+    List<Entry> entries = Arrays.asList(entry1, entry2);
+
+    ObjectHistory history = new ObjectHistory().withEntries(entries);
+
+    assertThat(history.getEntries(), hasSize(2));
+    assertThat(history.getEntries(), contains(entry1, entry2));
+  }
+
+  @Test
+  public void checkIfObjectHistoryWithEntryAppendedCreateAndAppendTheGivenEntriesCorrect() {
+    Entry entry1 = new Entry(Instant.now(), "eventCode1");
+    Entry entry2 = new Entry(Instant.now(), "eventCode2");
+
+    ObjectHistory history = new ObjectHistory();
+
+    history = history.withEntryAppended(entry1);
+    history = history.withEntryAppended(entry2);
+
+    assertThat(history.getEntries(), hasSize(2));
+    assertThat(history.getEntries(), contains(entry1, entry2));
+  }
+
+  @Test
+  public void checkIfEntryHasTimestampEventCodeAndSupplement() {
+    Instant timestamp = Instant.now();
+    Entry entry = new Entry(timestamp, "eventCode1", "supplement");
+
+    assertEquals(entry.getTimestamp(), timestamp);
+    assertEquals(entry.getEventCode(), "eventCode1");
+    assertEquals(entry.getSupplement(), "supplement");
+  }
+
+  @Test
+  public void throwIfSupplementIsNotSerializable() {
+    assertThrows(IllegalArgumentException.class,
+                 () -> new Entry(Instant.now(), "eventCode", new Object()));
+  }
+
+  @Test
+  public void checkIfEntryHasNoSupplement() {
+    Instant timestamp = Instant.now();
+    Entry entry = new Entry(timestamp, "eventCode1");
+
+    assertEquals(entry.getTimestamp(), timestamp);
+    assertEquals(entry.getEventCode(), "eventCode1");
+    assertEquals(entry.getSupplement(), "");
+  }
+
+  @Test
+  public void checkIfEntryHasTimestamp() {
+    Entry entry = new Entry("eventCode1", "supplement");
+
+    assertTrue(entry.getTimestamp() != null);
+    assertEquals(entry.getEventCode(), "eventCode1");
+    assertEquals(entry.getSupplement(), "supplement");
+  }
+
+  @Test
+  public void checkIfEntryHasTimestampAnNoSupplement() {
+    Entry entry = new Entry("eventCode1");
+
+    assertTrue(entry.getTimestamp() != null);
+    assertEquals(entry.getEventCode(), "eventCode1");
+    assertEquals(entry.getSupplement(), "");
+  }
+
+}
