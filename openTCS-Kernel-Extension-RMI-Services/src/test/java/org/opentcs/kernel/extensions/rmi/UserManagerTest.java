73e1e266d9e2ca0f0b1a1f8751fd0c3bd40a59ae
Gamze Culhalik <gamze.culhalik@iml.fraunhofer.de>
2023-11-22 13:46:45 +0000

    Add additional unit tests for UserManager
    Co-authored-by: Finja Averhaus <finja.averhaus@iml.fraunhofer.de>
    Merged-by: Martin Grzenia <martin.grzenia@iml.fraunhofer.de>

--- openTCS-Kernel-Extension-RMI-Services/src/test/java/org/opentcs/kernel/extensions/rmi/UserManagerTest.java
+++ openTCS-Kernel-Extension-RMI-Services/src/test/java/org/opentcs/kernel/extensions/rmi/UserManagerTest.java
@@ -10,20 +10,28 @@ package org.opentcs.kernel.extensions.rmi;
 import java.io.File;
 import java.util.EnumSet;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.BDDMockito.then;
 import org.opentcs.util.event.EventSource;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.aMapWithSize;
 import static org.hamcrest.Matchers.anEmptyMap;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.opentcs.access.rmi.ClientID;
+import org.opentcs.access.CredentialsException;
 
 /**
  * Unit tests for {@link UserManager}.
@@ -74,6 +82,85 @@ public class UserManagerTest {
   @AfterEach
   public void tearDown() {
     kernelExecutor.shutdown();
+  }
+
+  @Test
+  public void testIfUserManagerIsInitialized() {
+    assertThat(manager.isInitialized(), is(true));
+  }
+
+  @Test
+  public void testIfCleanerTaskIsTerminated() {
+    manager.terminate();
+
+    assertThat(manager.isInitialized(), is(false));
+    then(eventSource).should().unsubscribe(manager);
+  }
+
+  @Test
+  public void checkGetKnownUsers() {
+    assertThat(manager.getKnownUsers(), is(aMapWithSize(1)));
+    assertThat(manager.getKnownUsers(), hasEntry("peter", account1));
+  }
+
+  @Test
+  public void checkGetUserShouldReturnRightUser() {
+    assertThat(manager.getUser("peter"), is(account1));
+  }
+
+  @Test
+  public void checkGetUserShouldReturnNullForUnknownUser() {
+    assertNull(manager.getUser("marie"));
+  }
+
+  @Test
+  public void checkGetClientShouldReturnRightClient() {
+    manager.registerClient(id1, client1);
+
+    assertThat(manager.getClient(id1), is(client1));
+  }
+
+  @Test
+  public void checkGetClientShouldReturnNull() {
+    manager.registerClient(id1, client1);
+
+    assertNull(manager.getClient(new ClientID("jet")));
+  }
+
+  @Test
+  public void checkIfPollEventsReturnsTheCorrectEventList() {
+    manager.registerClient(id1, client1);
+
+    client1.getEventBuffer().setEventFilter(event -> true);
+
+    Object event1 = new Object();
+    manager.onEvent(event1);
+
+    List<Object> eventList = manager.pollEvents(id1, 0);
+
+    assertThat(eventList, hasSize(1));
+    assertThat(eventList, contains(event1));
+  }
+
+  @Test
+  public void checkVerifyCredentialsShouldThrowExceptionIfClientHasNoPermission() {
+    manager.registerClient(id1, client1);
+    assertThrows(CredentialsException.class,
+                 () -> manager.verifyCredentials(id1, UserPermission.SAVE_MODEL));
+  }
+
+  @Test
+  public void checkVerifyCredentialsShouldThrowExceptionIfClientDoesNotExist() {
+    assertThrows(CredentialsException.class,
+                 () -> manager.verifyCredentials(new ClientID("unknown-client"),
+                                                 UserPermission.SAVE_MODEL));
+  }
+
+  @Test
+  public void checkVerifyCredentialsShouldThrowNoException() {
+    manager.registerClient(id1, client1);
+    assertDoesNotThrow(
+        () -> manager.verifyCredentials(id1, UserPermission.READ_DATA));
   }
 
   @Test
