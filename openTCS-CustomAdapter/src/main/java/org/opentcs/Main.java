7b10011210e42e0543f686b7f5bff9e24f39d5a8
ms0708115 <ms0708115@gmail.com>
2024-06-27 14:18:37 +0800

    ADD: initial commit of custom Adapter

---
+++ openTCS-CustomAdapter/src/main/java/org/opentcs/Main.java
@@ -0,0 +1,11 @@
+package org.opentcs;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("Hello and welcome!");
+    for (int i = 1; i <= 5; i++) {
+
+      System.out.println("i = " + i);
+    }
+  }
+}
