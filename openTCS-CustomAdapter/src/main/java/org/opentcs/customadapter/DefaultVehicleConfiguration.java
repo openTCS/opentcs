fc2b6d40bdd5b6a15278fe7207cdf74c97b83fa1
ms0708115 <ms0708115@gmail.com>
2024-07-03 11:36:44 +0800

    Add license and class documentation to CustomVehicleCommAdapter
    The openTCS Authors' licensing information and expanded class documentation were added to the CustomVehicleCommAdapter. The class documentation provides a detailed description of the CustomVehicleCommAdapter which includes its purpose and the parameters necessary for its instantiation.

--- openTCS-CustomAdapter/src/main/java/org/opentcs/customadapter/DefaultVehicleConfiguration.java
+++ openTCS-CustomAdapter/src/main/java/org/opentcs/customadapter/DefaultVehicleConfiguration.java
@@ -1,16 +1,25 @@
+/**
+ * Copyright (c) The openTCS Authors.
+ *
+ * This program is free software and subject to the MIT license. (For details,
+ * see the licensing information (LICENSE.txt) you should have received with
+ * this copy of the software.)
+ */
+
 package org.opentcs.customadapter;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.concurrent.Executors;
+// import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * Default implementation of VehicleConfiguration interface.
  * Provides basic settings for vehicles without specific configurations.
  */
 public class DefaultVehicleConfiguration
-    implements VehicleConfigurationInterface {
+    implements
+      VehicleConfigurationInterface {
 
   private String host = "localhost";
   private int port = 502;
