f4fda17f10a84063bfd66dd2bea88a1e4eda350e
ms0708115 <ms0708115@gmail.com>
2024-07-10 14:02:56 +0800

    Enhance ModbusTCPVehicleCommAdapter with multiple features and classes
    Various improvements and feature additions have been made to the ModbusTCPVehicleCommAdapter. These include adding more classes like CMD1, CMD2, ModbusRegister, and ModbusCommand. Additional methods to support different vehicle operations have been added including movement command initialization and processing, more comprehensive Modbus command processing, and method for updating the vehicle's speed. Also, several properties have been added to represent a vehicle associated with a ModbusTCPVehicleCommAdapter like the vehicle's load state, position, station and velocity information and more.

--- openTCS-CustomAdapter/src/main/java/org/opentcs/customadapter/ModbusTCPVehicleCommAdapter.java
+++ openTCS-CustomAdapter/src/main/java/org/opentcs/customadapter/ModbusTCPVehicleCommAdapter.java
@@ -1,20 +1,26 @@
 package org.opentcs.customadapter;
 
+import static java.util.Objects.requireNonNull;
+
 import com.digitalpetri.modbus.master.ModbusTcpMaster;
 import com.digitalpetri.modbus.master.ModbusTcpMasterConfig;
 import com.digitalpetri.modbus.requests.ReadHoldingRegistersRequest;
 import com.digitalpetri.modbus.requests.WriteMultipleRegistersRequest;
 import com.digitalpetri.modbus.responses.ModbusResponse;
 import com.digitalpetri.modbus.responses.ReadHoldingRegistersResponse;
+import com.google.inject.assistedinject.Assisted;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+import java.beans.PropertyChangeEvent;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -24,9 +30,11 @@ import java.util.logging.Logger;
 import org.jgrapht.alg.util.Pair;
 import org.opentcs.customizations.kernel.KernelExecutor;
 import org.opentcs.data.model.Path;
+import org.opentcs.data.model.Point;
 import org.opentcs.data.model.Vehicle;
 import org.opentcs.data.order.DriveOrder;
 import org.opentcs.data.order.TransportOrder;
+import org.opentcs.drivers.vehicle.LoadHandlingDevice;
 import org.opentcs.drivers.vehicle.MovementCommand;
 import org.opentcs.drivers.vehicle.VehicleProcessModel;
 import org.opentcs.drivers.vehicle.management.VehicleProcessModelTO;
@@ -35,26 +43,95 @@ import org.opentcs.util.ExplainedBoolean;
 public class ModbusTCPVehicleCommAdapter
     extends
       CustomVehicleCommAdapter {
+
+  /**
+   * The name of the load handling device set by this adapter.
+   */
+  public static final String LHD_NAME = "default";
+  /**
+   * This class's Logger.
+   */
+  private static final Logger LOG = Logger.getLogger(ModbusTCPVehicleCommAdapter.class.getName());
+  /**
+   * An error code indicating that there's a conflict between a load operation and the vehicle's
+   * current load state.
+   */
+  private static final String LOAD_OPERATION_CONFLICT = "cannotLoadWhenLoaded";
+  /**
+   * An error code indicating that there's a conflict between an unload operation and the vehicle's
+   * current load state.
+   */
+  private static final String UNLOAD_OPERATION_CONFLICT = "cannotUnloadWhenNotLoaded";
+  /**
+   * Map of Modbus register address and function.
+   */
   private static final Map<String, ModbusTCPVehicleCommAdapter.ModbusRegister> REGISTER_MAP
       = new HashMap<>();
-  private static final Logger LOG = Logger.getLogger(ModbusTCPVehicleCommAdapter.class.getName());
-
-  // MAP1: absolute distance (mm) -> station name
+  /**
+   * MAP1: absolute distance (mm) -> station name.
+   */
   private final Map<Integer, String> map1 = new HashMap<>();
-  // MAP2: station name -> <CMD1, CMD2>
-  private final Map<String, Pair<CMD1, CMD2>> map2 = new ConcurrentHashMap<>();
-  // MC Pool
-  private final Set<MovementCommand> mcPool = new HashSet<>();
-
+  /**
+   * MAP2: station name -> <CMD1, CMD2>.
+   */
+  private Map<String, Pair<CMD1, CMD2>> map2 = new ConcurrentHashMap<>();
+  /**
+   * MovementCommand pool.
+   */
+  private Set<MovementCommand> mcPool = new HashSet<>();
+  /**
+   * Represents a vehicle associated with a ModbusTCPVehicleCommAdapter.
+   */
+  private final Vehicle vehicle;
+  /**
+   * The vehicle's load state.
+   */
+  private LoadState loadState = LoadState.EMPTY;
+  /**
+   * Map that represents the mapping between positions and stations.
+   * The key of the map is the position (integer) and the value is the corresponding station
+   * (string).
+   */
   private final Map<Integer, String> positionToStation = new HashMap<>();
+  /**
+   * Map that stores pairs of CMD1 and CMD2 objects associated with station names.
+   */
   private final Map<String, Pair<CMD1, CMD2>> stationToCommands = new HashMap<>();
+  /**
+   * The host address for the TCP connection.
+   */
   private final String host;
+  /**
+   * The port number for the TCP connection.
+   */
   private final int port;
+  /**
+   * Indicates whether the vehicle is currently connected.
+   */
   private boolean isConnected;
+  /**
+   * The maximum velocity for the vehicle.
+   */
   private int maxVelocity;
+  /**
+   * The current velocity of the vehicle.
+   */
   private int currentVelocity;
+  /**
+   * Represents a Modbus TCP master used for communication with Modbus TCP devices.
+   */
   private ModbusTcpMaster master;
+  /**
+   * The VelocityController class represents a controller for managing the velocity of a vehicle.
+   * It allows setting and retrieving the current velocity, as well as configuring the
+   * maximum acceleration, maximum deceleration, maximum forward velocity, and maximum reverse
+   * velocity.
+   */
   private final VelocityController velocityController;
+  /**
+   * Represents the state of a variable indicating whether it has been initialized.
+   */
+  private boolean initialized;
 
   /**
    * Initializes a new instance of ModbusTCPVehicleCommAdapter.
@@ -63,6 +140,7 @@ public class ModbusTCPVehicleCommAdapter
    * @param rechargeOperation The name of the recharge operation.
    * @param commandsCapacity The maximum capacity for storing commands.
    * @param executor The executor for scheduled tasks.
+   * @param vehicle The vehicle this adapter is associated with.
    * @param host The host address for the TCP connection.
    * @param port The port number for the TCP connection.
    */
@@ -73,12 +151,15 @@ public class ModbusTCPVehicleCommAdapter
       int commandsCapacity,
       @KernelExecutor
       ScheduledExecutorService executor,
+      @Assisted
+      Vehicle vehicle,
       String host,
       int port
   ) {
     super(processModel, rechargeOperation, commandsCapacity, executor);
     this.host = host;
     this.port = port;
+    this.vehicle = requireNonNull(vehicle, "vehicle");
     this.isConnected = false;
     this.currentVelocity = 0;
 
@@ -91,8 +172,65 @@ public class ModbusTCPVehicleCommAdapter
     this.velocityController = new VelocityController(
         maxAcceleration, maxDeceleration, maxFwdVelocity, maxRevVelocity
     );
-    // TODO: rename
-    initializeMap1();
+  }
+
+  @Override
+  public void initialize() {
+    if (isInitialized()) {
+      return;
+    }
+    super.initialize();
+
+    getProcessModel().setState(Vehicle.State.IDLE);
+    getProcessModel().setLoadHandlingDevices(
+        Arrays.asList(new LoadHandlingDevice(LHD_NAME, false))
+    );
+    initialized = true;
+  }
+
+  @Override
+  public boolean isInitialized() {
+    return initialized;
+  }
+
+  @Override
+  public void terminate() {
+    if (!isInitialized()) {
+      return;
+    }
+    super.terminate();
+    initialized = false;
+  }
+
+  /**
+   * Processes updates of the {@link CustomProcessModel}.
+   *
+   * <p><em>Overriding methods should also call this.</em></p>
+   *
+   * @param evt The property change event published by the model.
+   */
+  @Override
+  public void propertyChange(PropertyChangeEvent evt) {
+    super.propertyChange(evt);
+
+    if (!((evt.getSource()) instanceof CustomProcessModel)) {
+      return;
+    }
+    if (Objects.equals(
+        evt.getPropertyName(),
+        VehicleProcessModel.Attribute.LOAD_HANDLING_DEVICES.name()
+    )) {
+      if (!getProcessModel().getLoadHandlingDevices().isEmpty()
+          && getProcessModel().getLoadHandlingDevices().get(0).isFull()) {
+        loadState = LoadState.FULL;
+        // TODO: need change vehicle model size in future.
+//        getProcessModel().setLength(configuration.vehicleLengthLoaded());
+      }
+      else {
+        loadState = LoadState.EMPTY;
+//        getProcessModel().setLength(configuration.vehicleLengthUnloaded());
+      }
+    }
   }
 
   /**
@@ -107,6 +245,42 @@ public class ModbusTCPVehicleCommAdapter
     }
   }
 
+  /**
+   * Enables the communication adapter.
+   */
+  @Override
+  public synchronized void enable() {
+    if (isEnabled()) {
+      return;
+    }
+    super.enable();
+  }
+
+  /**
+   * Disables the communication adapter.
+   *
+   * <p>If the adapter is already disabled, the method returns without doing anything.
+   * When disabling the adapter, the vehicle's connection to the adapter is terminated,
+   * the adapter is marked as disabled, and the vehicle's state is set to UNKNOWN.</p>
+   *
+   * <p>This method is synchronized to ensure that only one thread can disable the adapter
+   * at a time.</p>
+   *
+   * @see CustomVehicleCommAdapter#disable()
+   */
+  @Override
+  public synchronized void disable() {
+    if (!isEnabled()) {
+      return;
+    }
+    super.disable();
+  }
+
+  /**
+   * Retrieves the custom process model associated with the vehicle.
+   *
+   * @return The custom process model.
+   */
   @Override
   @Nonnull
   public CustomProcessModel getProcessModel() {
@@ -120,6 +294,22 @@ public class ModbusTCPVehicleCommAdapter
       return;
     }
 
+
+    if (mcPool.isEmpty()) {
+      initializeMCPool(cmd);
+    }
+
+    if (mcPool.contains(cmd)) {
+      LOG.info("Skipping already processed command: ");
+      return;
+    }
+
+    mcPool.add(cmd);
+    processMovementCommand(cmd);
+
+    if (mcPool.size() == getSentCommands().size() + getUnsentCommands().size()) {
+      writeAllModbusCommands();
+    }
     // Set destination
     int destination = Integer.parseInt(cmd.getStep().getDestinationPoint().getName());
     sendModbusCommand("SET_DESTINATION", destination);
@@ -139,6 +329,117 @@ public class ModbusTCPVehicleCommAdapter
     // Verify commands were written correctly
     verifyModbusCommands();
   }
+
+  private void initializeMCPool(MovementCommand cmd) {
+    TransportOrder order = cmd.getTransportOrder();
+    List<DriveOrder> driveOrders = order.getAllDriveOrders();
+
+//    List<MovementCommand> allCommands = order.getAllDriveOrders().stream()
+//        .map(DriveOrder::getRoute)
+//        .flatMap(route -> route.getSteps().stream())
+//        .map(step -> createMovementCommand(order, step))
+//        .collect(Collectors.toList());
+
+
+    List<DriveOrder> relevantOrders = new ArrayList<>();
+
+    // Get the current DriveOrder
+    DriveOrder currentDriveOrder = order.getCurrentDriveOrder();
+    if (currentDriveOrder != null) {
+      relevantOrders.add(currentDriveOrder);
+    }
+
+    // Get future DriveOrders
+    relevantOrders.addAll(order.getFutureDriveOrders());
+
+    for (DriveOrder driveOrder : relevantOrders) {
+      // TODO: add movementCommands into mcpool.
+      LOG.info("DDDD");
+//      Route route = driveOrder.getRoute();
+//      if (route != null) {
+//        for (Route.Step step : route.getSteps()) {
+//          //Create a MovementCommand for each Step
+//          MovementCommand movementCmd = createMovementCommand(step, driveOrder);
+//          mcPool.add(movementCmd);
+//        }
+//      }
+    }
+
+    LOG.info(
+        String.format(
+            "Initialized MC pool for TransportOrder: %s. Pool size: %d", order.getName(), mcPool
+                .size()
+        )
+    );
+  }
+
+  private void processMovementCommand(MovementCommand cmd) {
+    Point destination = cmd.getStep().getDestinationPoint();
+    String stationName = map1.get(Integer.parseInt(destination.getName()));
+    if (stationName == null) {
+      throw new IllegalArgumentException("Cannot find station for the given destination point.");
+    }
+
+    CMD1 cmd1 = createCMD1(cmd);
+    CMD2 cmd2 = createCMD2(cmd);
+
+    map2.put(stationName, new Pair<>(cmd1, cmd2));
+  }
+
+  private void writeAllModbusCommands() {
+    List<ModbusCommand> commands = new ArrayList<>();
+    for (Map.Entry<String, Pair<CMD1, CMD2>> entry : map2.entrySet()) {
+      String stationName = entry.getKey();
+      Pair<CMD1, CMD2> cmds = entry.getValue();
+      int position = Integer.parseInt(stationName.substring(2)) * 1000;
+      commands.add(new ModbusCommand("CMD1", cmds.getFirst().toInt(), position));
+      commands.add(new ModbusCommand("CMD2", cmds.getSecond().toInt(), position + 1));
+    }
+    writeModbusCommands(commands);
+    mcPool.clear();
+    map2.clear();
+  }
+
+  @SuppressWarnings("checkstyle:TodoComment")
+  private CMD1 createCMD1(MovementCommand cmd) {
+    // TODO: MAKE IT REAL
+    return new CMD1(
+        0, 3, 4, cmd.getStep().getVehicleOrientation() == Vehicle.Orientation.FORWARD ? 0 : 1
+    );
+  }
+
+  private CMD2 createCMD2(MovementCommand cmd) {
+    // TODO: MAKE IT REA
+    return new CMD2(0, 0, 2);
+  }
+
+//  private MovementCommand createMovementCommand(Route.Step step, DriveOrder driveOrder) {
+//    TCSObjectReference<TransportOrder> transportOrder = driveOrder.getTransportOrder();
+//    String operation = driveOrder.getDestination().getOperation();
+//    Location opLocation = driveOrder.getDestination().getLocation();
+//
+//    // 判斷是否為最後一個移動命令
+//    boolean isFinalMovement = driveOrder.equals(transportOrder.getAllDriveOrders());
+//
+//    // 獲取最終目的地信息
+//    DriveOrder finalDriveOrder = transportOrder.getFinalDriveOrder();
+//    Location finalDestinationLocation = finalDriveOrder.getDestination().getLocation();
+//    Point finalDestination = finalDriveOrder.getDestination().getDestination();
+//    String finalOperation = finalDriveOrder.getDestination().getOperation();
+//
+//    return new MovementCommand(
+//        transportOrder,
+//        driveOrder,
+//        step,
+//        operation,
+//        opLocation,
+//        isFinalMovement,
+//        finalDestinationLocation,
+//        finalDestination,
+//        finalOperation,
+//        transportOrder.getProperties()
+//    );
+//  }
 
   /**
    * Converts a TransportOrder to a list of ModbusCommands.
@@ -443,7 +744,7 @@ public class ModbusTCPVehicleCommAdapter
   @Nonnull
   @Override
   public ExplainedBoolean canProcess(@Nonnull
-  org.opentcs.data.order.TransportOrder order) {
+  TransportOrder order) {
     return new ExplainedBoolean(true, "ModbusTCP adapter can process all orders.");
   }
 
@@ -470,8 +771,9 @@ public class ModbusTCPVehicleCommAdapter
   // Add a method to update the vehicle's speed
   public void updateVehicleSpeed(int newSpeed) {
     velocityController.setCurrentVelocity(newSpeed);
+    currentVelocity = newSpeed;
     // TODO: overload a double type parameter version of sendModbusCommand for speed.
-    sendModbusCommand("SET_SPEED", (int) velocityController.getCurrentVelocity());
+    sendModbusCommand("SET_SPEED", (int) currentVelocity);
   }
 
   static {
@@ -564,4 +866,11 @@ public class ModbusTCPVehicleCommAdapter
     }
   }
 
+  /**
+   * The vehicle's possible load states.
+   */
+  private enum LoadState {
+    EMPTY,
+    FULL;
+  }
 }
