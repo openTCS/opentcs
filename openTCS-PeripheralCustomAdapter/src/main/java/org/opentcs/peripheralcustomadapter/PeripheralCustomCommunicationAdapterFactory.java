9603463fb4db023302710a5805b56092b0b3b405
Sean Chang <t0970993752@gmail.com>
2024-07-29 14:53:57 +0800

    Add PeripheralCustomCommunicationAdapterFactory Interface

---
+++ openTCS-PeripheralCustomAdapter/src/main/java/org/opentcs/peripheralcustomadapter/PeripheralCustomCommunicationAdapterFactory.java
@@ -0,0 +1,8 @@
+package org.opentcs.peripheralcustomadapter;
+
+import org.opentcs.drivers.peripherals.PeripheralCommAdapterFactory;
+
+public interface PeripheralCustomCommunicationAdapterFactory
+    extends
+      PeripheralCommAdapterFactory {
+}
