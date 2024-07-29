0c73a55a7a5d7000f3c65474ba24453aba67766f
Sean Chang <t0970993752@gmail.com>
2024-07-29 17:02:59 +0800

    Add PeripheralCustomCommunicationAdapterDescription

---
+++ openTCS-PeripheralCustomAdapter/src/main/java/org/opentcs/peripheralcustomadapter/PeripheralCustomCommunicationAdapterDescription.java
@@ -0,0 +1,27 @@
+package org.opentcs.peripheralcustomadapter;
+
+import jakarta.annotation.Nonnull;
+import org.opentcs.drivers.peripherals.PeripheralCommAdapterDescription;
+
+/**
+ * A {@link PeripheralCustomCommunicationAdapterDescription} for no comm adapter.
+ */
+
+public class PeripheralCustomCommunicationAdapterDescription
+    extends
+      PeripheralCommAdapterDescription {
+
+  /**
+   * Creates a new instance.
+   */
+  public PeripheralCustomCommunicationAdapterDescription() {
+  }
+
+  @Nonnull
+  @Override
+  public String getDescription() {
+    {
+      return "Peripheral Custom Communication Adapter";
+    }
+  }
+}
