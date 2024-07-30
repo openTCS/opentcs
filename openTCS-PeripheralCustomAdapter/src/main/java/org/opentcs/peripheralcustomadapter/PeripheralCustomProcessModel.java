14c621ec002e6875796033e752bc32f15e852256
shuhuachang <t0970993752@gmail.com>
2024-07-30 23:01:15 +0800

    Add PeripheralCustomProcessModel

---
+++ openTCS-PeripheralCustomAdapter/src/main/java/org/opentcs/peripheralcustomadapter/PeripheralCustomProcessModel.java
@@ -0,0 +1,75 @@
+package org.opentcs.peripheralcustomadapter;
+
+import java.io.Serializable;
+import org.opentcs.data.model.Location;
+import org.opentcs.data.model.PeripheralInformation;
+import org.opentcs.data.model.TCSResourceReference;
+import org.opentcs.drivers.peripherals.PeripheralProcessModel;
+
+/**
+ * The process model for the peripheral communication adapter.
+ */
+public class PeripheralCustomProcessModel
+    extends
+      PeripheralProcessModel
+    implements
+      Serializable {
+
+  /**
+   * Creates a new instance.
+   *
+   * @param location The reference to the location this adapter is attached to.
+   */
+  public PeripheralCustomProcessModel(TCSResourceReference<Location> location) {
+    this(location, false, false, PeripheralInformation.State.UNKNOWN);
+  }
+
+  private PeripheralCustomProcessModel(
+      TCSResourceReference<Location> location,
+      boolean commAdapterEnabled,
+      boolean commAdapterConnected,
+      PeripheralInformation.State state
+  ) {
+    super(location, commAdapterEnabled, commAdapterConnected, state);
+  }
+
+  @Override
+  public PeripheralCustomProcessModel withLocation(TCSResourceReference<Location> location) {
+    return new PeripheralCustomProcessModel(
+        location,
+        isCommAdapterEnabled(),
+        isCommAdapterConnected(),
+        getState()
+    );
+  }
+
+  @Override
+  public PeripheralCustomProcessModel withCommAdapterEnabled(boolean commAdapterEnabled) {
+    return new PeripheralCustomProcessModel(
+        getLocation(),
+        commAdapterEnabled,
+        isCommAdapterConnected(),
+        getState()
+    );
+  }
+
+  @Override
+  public PeripheralCustomProcessModel withCommAdapterConnected(boolean commAdapterConnected) {
+    return new PeripheralCustomProcessModel(
+        getLocation(),
+        isCommAdapterEnabled(),
+        commAdapterConnected,
+        getState()
+    );
+  }
+
+  @Override
+  public PeripheralCustomProcessModel withState(PeripheralInformation.State state) {
+    return new PeripheralCustomProcessModel(
+        getLocation(),
+        isCommAdapterEnabled(),
+        isCommAdapterConnected(),
+        state
+    );
+  }
+}
