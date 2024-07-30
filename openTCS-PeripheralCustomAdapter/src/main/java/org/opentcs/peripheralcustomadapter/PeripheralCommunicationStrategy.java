fbe3594ca92a27f1b1b5bcc3b4f54453212b0e2e
Sean Chang <t0970993752@gmail.com>
2024-07-30 14:50:45 +0800

    Add PeripheralCommunicationStrategy

---
+++ openTCS-PeripheralCustomAdapter/src/main/java/org/opentcs/peripheralcustomadapter/PeripheralCommunicationStrategy.java
@@ -0,0 +1,65 @@
+package org.opentcs.peripheralcustomadapter;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.logging.Logger;
+import org.opentcs.customizations.kernel.KernelExecutor;
+import org.opentcs.data.model.Location;
+import org.opentcs.data.model.TCSResourceReference;
+import org.opentcs.util.event.EventHandler;
+
+public class PeripheralCommunicationStrategy
+    implements
+      PeripheralCustomAdapterComponentsFactory {
+  private static final Logger LOG = Logger.getLogger(
+      PeripheralCommunicationStrategy.class.getName()
+  );
+
+  private final Map<String, Provider<StrategyCreator>> strategyProviders;
+  private final PeripheralDeviceConfigurationProvider configProvider;
+  private final ScheduledExecutorService executor;
+  private final EventHandler eventHandler;
+
+  @Inject
+  PeripheralCommunicationStrategy(
+      Map<String, Provider<StrategyCreator>> strategyProviders,
+      @KernelExecutor
+      ScheduledExecutorService executor,
+      EventHandler eventHandler
+  ) {
+    this.strategyProviders = strategyProviders;
+    this.executor = executor;
+    this.configProvider = new PeripheralDeviceConfigurationProvider();
+    this.eventHandler = eventHandler;
+  }
+
+  @Override
+  public PeripheralCommunicationAdapter createPeripheralCustomCommAdapter(
+      TCSResourceReference<Location> location
+  ) {
+    PeripheralDeviceConfiguration config = configProvider.getConfiguration(location.getName());
+    if (config == null) {
+      if (String.CASE_INSENSITIVE_ORDER.compare(location.getName(), "SAA-mini-OHT-Sensor0001")
+          == 0) {
+        config = new PeripheralDeviceConfiguration("ModbusTCP", "192.168.1.20", 502);
+        configProvider.setConfiguration(location.getName(), config);
+      }
+      else {
+        config = new PeripheralDeviceConfiguration("ModbusTCP", "192.168.1.21", 502);
+        configProvider.setConfiguration(location.getName(), config);
+      }
+    }
+
+    String strategyKey = config.currentStrategy();
+    Provider<StrategyCreator> creatorProvider = strategyProviders.get(strategyKey);
+    if (creatorProvider == null) {
+      LOG.warning("Unknown strategy: " + strategyKey + ". Using default ModbusTCP strategy.");
+      creatorProvider = strategyProviders.get("ModbusTCP");
+    }
+
+    StrategyCreator creator = creatorProvider.get();
+    return creator.createAdapter(location, eventHandler, executor, config);
+  }
+}
