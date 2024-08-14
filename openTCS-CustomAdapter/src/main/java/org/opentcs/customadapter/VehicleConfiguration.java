51cabab47dc3639a64007c8d5b2278dc8653cb6b
Eric Fu <EricCL.Fu@saa-symtek.com>
2024-08-14 11:23:33 +0800

    UPDATE: add initial pose in configure

--- openTCS-CustomAdapter/src/main/java/org/opentcs/customadapter/VehicleConfiguration.java
+++ openTCS-CustomAdapter/src/main/java/org/opentcs/customadapter/VehicleConfiguration.java
@@ -1,6 +1,8 @@
 package org.opentcs.customadapter;
 
-public record VehicleConfiguration(String currentStrategy, String host, int port) {
+public record VehicleConfiguration(
+    String currentStrategy, String host, int port, String initialPose
+) {
   public VehicleConfiguration {
     if (port < 0 || port > 65535) {
       throw new IllegalArgumentException("Invalid port number, port number range is 0 - 65535");
