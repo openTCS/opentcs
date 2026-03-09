ce964b6a8a9b99a54d427a4fec5b34e556ae286d
Stefan Walter <stefan.walter@iml.fraunhofer.de>
2026-03-09 09:47:20 +0100

    Document event handling context
    Document that methods in EventHandler and EventSource are supposed to be
    called via the kernel executor (if used within the kernel application).
    
    Merged-by: Martin Grzenia <martin.grzenia@iml.fraunhofer.de>

--- opentcs-api-base/src/main/java/org/opentcs/util/event/EventHandler.java
+++ opentcs-api-base/src/main/java/org/opentcs/util/event/EventHandler.java
@@ -9,8 +9,15 @@ public interface EventHandler {
 
   /**
    * Processes the event object.
+   * <p>
+   * When used within the kernel application, callers of this method are supposed to ensure that the
+   * call is executed in the context of the kernel executor. Implementations of this method, on
+   * the other hand, need to take into account that they are executed in the context of the kernel
+   * executor.
+   * </p>
    *
    * @param event The event object.
+   * @see org.opentcs.customizations.kernel.KernelExecutor
    */
   void onEvent(Object event);
 }
--- opentcs-api-base/src/main/java/org/opentcs/util/event/EventSource.java
+++ opentcs-api-base/src/main/java/org/opentcs/util/event/EventSource.java
@@ -9,15 +9,29 @@ public interface EventSource {
 
   /**
    * Subscribes the given listener to events emitted by this source.
+   * <p>
+   * When used within the kernel application, callers of this method are supposed to ensure that the
+   * call is executed in the context of the kernel executor. Implementations of this method, on
+   * the other hand, need to take into account that they are executed in the context of the kernel
+   * executor.
+   * </p>
    *
    * @param listener The listener to be subscribed.
+   * @see org.opentcs.customizations.kernel.KernelExecutor
    */
   void subscribe(EventHandler listener);
 
   /**
    * Unsubscribes the given listener.
+   * <p>
+   * When used within the kernel application, callers of this method are supposed to ensure that the
+   * call is executed in the context of the kernel executor. Implementations of this method, on
+   * the other hand, need to take into account that they are executed in the context of the kernel
+   * executor.
+   * </p>
    *
    * @param listener The listener to be unsubscribed.
+   * @see org.opentcs.customizations.kernel.KernelExecutor
    */
   void unsubscribe(EventHandler listener);
 }
